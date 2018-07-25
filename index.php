afdcac62991a7c094cecf1bfbb4b0718349e8634
SinesiphoLurafu <40822924+SinesiphoLurafu@users.noreply.github.com>
2018-07-25 14:03:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    
+
+   
+
+
+    <form  method = "POST">
+    <div>
+        <p>It's Quiz Time!</p>
+    </div>
+    <fieldset>
+    <legend>1. What is the third sign of the Zodiac?</legend>
+    
+    <li><input type="radio" value = "capricorn" name = "one"> Capricorn.</li>
+    
+    <li><input type="radio" value = "gemini" name = "one"> Gemini.</li>
+    
+    <li><input type="radio" value = "aries" name = "one"> Aries.</li>
+    
+   <li> <input type="radio" value = "scorpio" name = "one"> Scorpio.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>2. What is the largest bone in the human body?</legend>
+    
+    <li><input type="radio" value = "skull" name = "two"> Skull.</li>
+    
+    <li><input type="radio" value = "pelvic_gurdle" name = "two"> Pelvic gurdle.</li>
+    
+    <li><input type="radio" value = "femur" name = "two"> Femur.</li>
+    
+   <li> <input type="radio" value = "ulna" name = "two"> Ulna.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>3. What is the largest two digit prime number?</legend>
+    
+    <li><input type="radio" value = "99" name = "three"> 99.</li>
+    
+    <li><input type="radio" value = "95" name = "three"> 95.</li>
+    
+    <li><input type="radio" value = "97" name = "three"> 97.</li>
+    
+   <li> <input type="radio" value = "91" name = "three"> 91.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>4. What is the square root of 256?</legend>
+    
+    <li><input type="radio" value = "18" name = "four"> 18.</li>
+    
+    <li><input type="radio" value = "19" name = "four"> 19.</li>
+    
+    <li><input type="radio" value = "23" name = "four"> 23.</li>
+    
+   <li> <input type="radio" value = "16" name = "four"> 16.</li>
+    </fieldset>
+
+    <br>
+    <fieldset>
+    <legend>5. What is the collective name for a group of lions?</legend>
+    
+    <li><input type="radio" value = "School" name = "five"> School.</li>
+    
+    <li><input type="radio" value = "Swarm" name = "five"> Swarm.</li>
+    
+    <li><input type="radio" value = "Pride" name = "five"> Pride.</li>
+    
+   <li> <input type="radio" value = "Herd" name = "five"> Herd.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>6. How many legs does a lobster have?</legend>
+    
+    <li><input type="radio" value = "8" name = "six"> 8.</li>
+    
+    <li><input type="radio" value = "12" name = "six"> 12.</li>
+    
+    <li><input type="radio" value = "10" name = "six"> 10.</li>
+    
+   <li> <input type="radio" value = "6" name = "six"> 6.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>7. "Murder" is the collective noun for a group of which bird?</legend>
+    
+    <li><input type="radio" value = "robins" name = "seven"> Robins.</li>
+    
+    <li><input type="radio" value = "crows" name = "seven"> Crows.</li>
+    
+    <li><input type="radio" value = "parrots" name = "seven"> Parrots.</li>
+    
+   <li> <input type="radio" value = "vultures" name = "seven"> Vultures.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>8. Where in the human body would you find the medulla oblongata?</legend>
+    
+    <li><input type="radio" value = "brain" name = "eight"> The Brain.</li>
+    
+    <li><input type="radio" value = "intestine" name = "eight"> The intestines.</li>
+    
+    <li><input type="radio" value = "heart" name = "eight"> The heart.</li>
+    
+   <li> <input type="radio" value = "eyes" name = "eight"> The eyes.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>9. Which Jacobean dramatist wrote "The Alchemist"?</legend>
+    
+    <li><input type="radio" value = "ben" name = "nine"> Ben Jonson.</li>
+    
+    <li><input type="radio" value = "paulo" name = "nine"> Paulo Coelho.</li>
+    
+    <li><input type="radio" value = "shakespeare" name = "nine"> William Shakespeare.</li>
+    
+   <li> <input type="radio" value = "john" name = "nine"> John Kani.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>10. Ankara is the capital city of which country?</legend>
+    
+    <li><input type="radio" value = "andorra" name = "ten"> Andorra.</li>
+    
+    <li><input type="radio" value = "turkey" name = "ten"> Turkey.</li>
+    
+    <li><input type="radio" value = "morocco" name = "ten"> Morocco.</li>
+    
+   <li> <input type="radio" value = "burundi" name = "ten"> Burundi.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>11. Give another name for the study of fossils?</legend>
+    
+    <li><input type="radio" value = "paleontology" name = "eleven"> Paleontology.</li>
+    
+    <li><input type="radio" value = "fossiology" name = "eleven"> Fossiology.</li>
+    
+    <li><input type="radio" value = "archaelogy" name = "eleven"> Archaelogy.</li>
+    
+   <li> <input type="radio" value = "panthology" name = "eleven"> Panthology.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>12. What is called a fish with a snake-like body?</legend>
+    
+    <li><input type="radio" value = "waterfish" name = "twelve"> Water fish.</li>
+    
+    <li><input type="radio" value = "eel" name = "twelve"> Eel.</li>
+    
+    <li><input type="radio" value = "sardine" name = "twelve"> Sardine.</li>
+    
+   <li> <input type="radio" value = "Turtle" name = "twelve"> Turtle.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>13. What is the islamic term for fast?</legend>
+    
+    <li><input type="radio" value = "eid" name = "thirteen"> Eid Mubarack.</li>
+    
+    <li><input type="radio" value = "ramadan" name = "thirteen"> Ramadan.</li>
+    
+    <li><input type="radio" value = "punjab" name = "thirteen"> Punjab.</li>
+    
+   <li> <input type="radio" value = "quran" name = "thirteen"> Quran.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>14. What word is used in Hawaii both to greet and to say goodbye?</legend>
+    
+    <li><input type="radio" value = "bonjour" name = "fourteen"> Bonjour.</li>
+    
+    <li><input type="radio" value = "aloha" name = "fourteen"> Aloha.</li>
+    
+    <li><input type="radio" value = "obrigado" name = "fourteen"> Obrigado.</li>
+    
+   <li> <input type="radio" value = "halo" name = "fourteen"> Halo.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>15. What is the name of the German airline?	</legend>
+    
+    <li><input type="radio" value = "lufthansa" name = "fifteen"> Lufthansa Airways.</li>
+    
+    <li><input type="radio" value = "jet" name = "fifteen"> Jet Airways.</li>
+    
+    <li><input type="radio" value = "kingfisher" name = "fifteen"> Kingfisher Airways.</li>
+    
+   <li> <input type="radio" value = "regent" name = "fifteen"> Regent Airways.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>16. How many stars has the American flag got?</legend>
+
+    <li><input type="radio" value = "forty" name = "sixteen"> 40.</li>
+    
+    <li><input type="radio" value = "thirty" name = "sixteen"> 30.</li>
+    
+    <li><input type="radio" value = "fifty" name = "sixteen"> 50.</li>
+    
+   <li> <input type="radio" value = "sixty" name = "sixteen"> 60.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>17. According to the Bible, who was the first murderer?</legend>
+    
+    <li><input type="radio" value = "cain" name = "seventeen"> Cain.</li>
+    
+    <li><input type="radio" value = "abel" name = "seventeen"> Abel.</li>
+    
+    <li><input type="radio" value = "abraham" name = "seventeen"> Abraham.</li>
+    
+   <li> <input type="radio" value = "josephs_brothers" name = "seventeen"> Joseph's Ten brothers.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>18. In what year did princess diana die?</legend>
+    
+    <li><input type="radio" value = "1997" name = "eighteen"> 1997.</li>
+    
+    <li><input type="radio" value = "1998" name = "eighteen"> 1998.</li>
+    
+    <li><input type="radio" value = "1996" name = "eighteen"> 1996</li>
+    
+   <li> <input type="radio" value = "1999" name = "eighteen"> 1999.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>19. What is the name of the gas that let rise bread dough?</legend>
+    
+    <li><input type="radio" value = "carbon" name = "nineteen"> Carbon Dioxide.</li>
+    
+    <li><input type="radio" value = "helium" name = "nineteen"> Helium.</li>
+    
+    <li><input type="radio" value = "hydrogen" name = "nineteen"> Hydrogen.</li>
+    
+   <li> <input type="radio" value = "nitrogen" name = "nineteen"> Nitrogen.</li>
+    </fieldset>
+    <br>
+    <fieldset>
+    <legend>20. Which cheese is traditionally used for pizzas?</legend>
+    
+    <li><input type="radio" value = "mozzarella" name = "twenty"> Mozzarella.</li>
+    
+    <li><input type="radio" value = "cheddar" name = "twenty"> Cheddar.</li>
+    
+    <li><input type="radio" value = "goudda" name = "twenty"> Goudda.</li>
+    
+   <li> <input type="radio" value = "goat" name = "twenty"> Goat Cheese.</li>
+    </fieldset>
+    <input type="submit">
+    
+    </form>
+    <?
+         $correct = 0;
+         $ans1 = $_POST['one'];
+         $ans2 = $_POST['two'];
+         $ans3 = $_POST['three'];
+         $ans4 = $_POST['four'];
+         $ans5 = $_POST['five'];
+         $ans6 = $_POST['six'];
+         $ans7 = $_POST['seven'];
+         $ans8 = $_POST['eight'];
+         $ans9 = $_POST['nine'];
+         $ans10 = $_POST['ten'];
+         $ans11 = $_POST['eleven'];
+         $ans12 = $_POST['twelve'];
+         $ans13 = $_POST['thirteen'];
+         $ans14 = $_POST['fourteen'];
+         $ans15 = $_POST['fifteen'];
+         $ans16 = $_POST['sixteen'];
+         $ans17 = $_POST['seventeen'];
+         $ans18 = $_POST['eighteen'];
+         $ans19 = $_POST['nineteen'];
+         $ans20 = $_POST['twenty'];
+     
+         
+          if ($ans1 == "gemini") {
+              $correct++;
+          }
+          if ($ans2 == "femur") {
+              $correct++;
+          }
+          if ($ans3 == "97") {
+              $correct++;
+          }
+          if ($ans4 == "16") {
+              $correct++;
+          }
+          if ($ans5 == "Pride") {
+              $correct++;
+          }
+          if ($ans6 == "10") {
+              $correct++;
+          }
+          if ($ans7 == "crows") {
+              $correct++;
+          }
+          if ($ans8 == "brain") {
+              $correct++;
+          }
+          if ($ans9 == "ben") {
+              $correct++;
+          }
+          if ($ans10 == "turkey") {
+              $correct++;
+          }
+          if ($ans11 == "paleontology") {
+              $correct++;
+          }
+          if ($ans12 == "eel") {
+              $correct++;
+          }
+          if ($ans13 == "ramadan") {
+              $correct++;
+          }
+          if ($ans14 == "aloha") {
+              $correct++;
+          }
+          if ($ans15 == "lufthansa") {
+              $correct++;
+          }
+          if ($ans16 == "fifty") {
+              $correct++;
+          }
+          if ($ans17 == "cain") {
+              $correct++;
+          }
+          if ($ans18 == "1997") {
+              $correct++;
+          }
+          if ($ans19 == "carbon") {
+              $correct++;
+          }
+          if ($ans20 == "mozzarella") {
+              $correct++;
+          }
+          
+          if ($correct < 10) {
+              echo "Thank you for completing the quiz! Your score was ".$correct. "/20. Please try again to get better results.";
+          } elseif($correct>= 10 && $correct < 15) {
+            echo "Thank you for completing the quiz! Your score was ". $correct. "/20. You have scored averagely.";
+          }else{
+            echo "Thank you for completing the quiz! Your score was ".$correct. "/20. Congratulations! You did very well!";
+          }
+          
+          
+          
+         
+        ?> 
+
+         
+   
+   
+</body>
+</html>
